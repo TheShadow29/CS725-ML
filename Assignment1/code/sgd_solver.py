75ff5f6a438d4cf70c0c5cd262d366c567791824
Arka Sadhu <ark.sadhu2904@gmail.com>
2017-09-02 16:13:09 +0530

    Damn, now I need to start over with the template file

--- Assignment1/code/sgd_solver.py
+++ Assignment1/code/sgd_solver.py
@@ -8,7 +8,8 @@ class one_train_info():
     def __init__(self, param_list):
         self.id1 = param_list[0]
         self.output = param_list[-1]
-        self.all_other_params = param_list[1:-1]
+        # Note to self : not taking date and time into consideration
+        self.all_other_params = param_list[2:-1]
         return
 
     def show_param(self, param):
@@ -30,15 +31,61 @@ class reader(object):
         return
 
 
-class model(object):
-    def __init__(self, lamb, p, phi):
-        self.lamb = lamb
-        self.p = p
-        self.phi = np.append(phi, 1)
-        # Reminder to not forget the bias
-        self.weights = np.zeros(self.phi.shape)
+class phi_holder(object):
+    def __init__(self, _phi, _id_list):
+        self.phi = _phi
+        self.id_list = _id_list
 
-    # def calculate_loss(self):
+
+class model(object):
+    # Might also need an id_list to ind map
+    def __init__(self, _lamb, _p, all_data):
+        self.lamb = _lamb
+        self.p = _p
+        observed_y = list()
+        id_list = list()
+        # +1 is for the bias term
+        phi_matrix = np.zeros((len(all_data.all_info_list),
+                              len(all_data.all_info_list[0].all_other_params) + 1))
+        for ind, d_info in enumerate(all_data.all_info_list):
+            # d_info is of the type one_train_info
+            id_list.append(d_info.id1)
+            observed_y.append(d_info.output)
+            phi_matrix[ind, :] = np.append(d_info.all_other_params, 1)
+
+        self.id_np_list = np.array(id_list)
+        self.y_obs = np.array(observed_y)
+        self.Phi_h = phi_holder(phi_matrix, id_list)
+        # Reminder to not forget the bias
+        self.weights = np.zeros(self.Phi_h.phi.shape[1])
+        self.total_ind = self.Phi_h.phi.shape[0]
+
+        self.lr = 1
+
+    def complete_loss(self):
+        norm2_error = 0
+        for i in range(self.total_ind):
+            norm2_error += self.norm2_error_one_ind(i)**2
+
+        penalty = self.lamb * np.linalg.norm(self.weights, ord=self.p)
+        return norm2_error + penalty
+
+    def norm2_error_one_ind(self, ind):
+        # note that it is not norm2 squared [Need to be careful]
+        # currently accepting index, may need to change if it should accept the ID
+        return np.abs(np.dot(self.Phi_h.phi[ind, :], self.weights) - self.y_obs[ind])
+
+    def one_weight_update(self, x_ind):
+        new_weights = self.weights - self.lr * self.gradient_one_point(x_ind)
+        return new_weights
+
+    def gradient_one_point(self, x_ind):
+        grad = np.zeros(self.weights.shape)
+        for i in range(grad.shape):
+            grad[i] = 2 * norm2_error_one_ind(x_ind) +
+
+    def penalty_grad(self, w_ind):
+
 
 
 if __name__ == '__main__':
@@ -47,3 +94,8 @@ if __name__ == '__main__':
     # Regression with p-norm regularization
     # loss = ||y - y'||^2 + lambda * ||w||^p
     # Can try to use k-fold cross validation
+    # to check if the sgd code is indeed working
+    # do a sanity check for the case p=2
+    lamb = 1
+    p = 2
+    sgd_model = model(lamb, p, all_data)
