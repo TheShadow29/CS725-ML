c26f9dc4daf64cca85ad19279ce15647c27d2210
Arka Sadhu <ark.sadhu2904@gmail.com>
2017-09-05 18:42:16 +0530

    Rectified a big error in handling array, oh gawd!!

--- Assignment1/code/sgd_solver.py
+++ Assignment1/code/sgd_solver.py
@@ -6,6 +6,7 @@ import pdb
 from sgd_template import get_feature_matrix, get_output
 import matplotlib.pyplot as plt
 import re
+import pickle
 import warnings
 warnings.filterwarnings("error")
 # class one_train_info():
@@ -64,7 +65,7 @@ class model(object):
         # self.Phi_h = phi_holder(phi_matrix, self.id_np_list)
         # Reminder to not forget the bias
         self.phi_matrix = feature_matrix
-        self.weights = np.zeros(self.phi_matrix.shape[1])
+        self.weights = np.zeros((self.phi_matrix.shape[1], 1))
         self.total_ind = self.phi_matrix.shape[0]
 
         self.lr = 1e-8
@@ -120,7 +121,11 @@ class model(object):
     def sgd(self, nit=5, batch=1e2):
         # Stochastic gradient descent
         # Assuming no shuffling for now
-
+        if self.p == 2:
+            return self.p2_direct_solve()
+        if self.p == 1:
+            self.p1_solver()
+            return
         assert type(batch) is int
         curr_loss = self.complete_loss()
         prev_loss = curr_loss + 10000
@@ -180,7 +185,9 @@ class model(object):
         return np.dot(tmp_mat2, tmp_mat3)
 
     def p2_direct_solve(self):
+        # pdb.set_trace()
         self.weights = self.p2_solver()
+        # pdb.set_trace()
         return
 
     def valid_accuracy(self, valid_phi, valid_output):
@@ -202,6 +209,9 @@ class model(object):
             str_to_write += str_format.format(tid, str(y_pred[0]))
         with open('../eval/' + out_fname, 'w') as f:
             f.write(str_to_write)
+        with open('../eval/' + out_fname[:-4] + '.pkl', 'wb') as g:
+            pickle.dump(self.weights, g)
+        return
 
     def soft_function(self, y_arr, lamb):
         out_vec = np.zeros(y_arr.shape)
@@ -220,7 +230,7 @@ class model(object):
         k = 0
         # eps = 1e-10
         eig_values, eig_vecs = np.linalg.eig(np.dot(self.phi_matrix.T, self.phi_matrix))
-        alpha = np.max(eig_values)
+        alpha = 1.1 * np.max(eig_values)
         curr_weights = self.weights
         prev_weights = curr_weights + 10
         # while np.linalg.norm(prev_weights - curr_weights) > eps:
@@ -231,56 +241,57 @@ class model(object):
             prev_weights = curr_weights
             tmp1 = self.weights
             tmp2 = np.dot(self.phi_matrix.T, self.y_obs - np.dot(self.phi_matrix, self.weights))
+            # pdb.set_trace()
             tmp3 = tmp1 + tmp2 / alpha
             curr_weights = self.soft_function(tmp3, self.lamb / (2 * alpha))
             self.weights = curr_weights
             print('Iter', k, 'loss', self.complete_loss())
         return
 
-def plotter():
-    dat_reader = pd.read_csv('../data/train.csv', sep=',')
+# def plotter():
+#     dat_reader = pd.read_csv('../data/train.csv', sep=',')
 
-    x_ax = np.array(pat.findall('\n'.join(dat_reader['date'])), dtype='int')
-    pdb.set_trace()
-    # for x in x_ax:
-    #     x_ax_new.append(int(x.split(' ')[-1].split(':')[0]))
+#     x_ax = np.array(pat.findall('\n'.join(dat_reader['date'])), dtype='int')
+#     pdb.set_trace()
+#     # for x in x_ax:
+#     #     x_ax_new.append(int(x.split(' ')[-1].split(':')[0]))
 
-    plt.plot(x_ax[:, 3], dat_reader['Output'])
-    plt.show()
+#     plt.plot(x_ax[:, 3], dat_reader['Output'])
+#     plt.show()
 
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
 
-    # pat = re.compile(r'(\d*)-(\d*)-(\d*)\s(\d*):(\d*):(\d*)')
-    # plotter()
-    # pdb.set_trace()
-    fname = '../data/train.csv'
-    # fname = '../data/test_features.csv'
-    feature_matrix = get_feature_matrix(fname)
-    out_vec = get_output(fname)
-    lambda_reg = 1
-    p = 1
-    # weight_vector = get_weight_vector(feature_matrix, out_vec, lambda_reg, p)
-    # Regression with p-norm regularization
-    # loss = ||y - y'||^2 + lambda * ||w||^p
-    # Can try to use k-fold cross validation
-    # to check if the sgd code is indeed working
-    # do a sanity check for the case p=2
-    # sgd_model = model(lamb, p, all_data)
-    tot_points = feature_matrix.shape[0]
-    tr_pts = int(tot_points * 0.9)
-    sgd_model = model(feature_matrix[:tr_pts], out_vec[:tr_pts], lambda_reg, p)
-    sgd_model.p2_direct_solve()
-    # sgd_model.sgd(1000, 1000)
-    sgd_model.p1_solver()
-    print(sgd_model.complete_loss())  #
-    # test_feature_mat = get_feature_matrix('../data/test_features.csv')
-    # sgd_model.p2_direct_solve()
-    sgd_model.valid_accuracy(feature_matrix[tr_pts:], out_vec[tr_pts:])
-    # sgd_model.valid_accuracy(feature_matrix, out_vec)
-    test_fname = '../data/test_features.csv'
-    test_data_reader = pd.read_csv(test_fname, sep=',')
-    test_id_list = test_data_reader['Id']
-    test_feature_mat = get_feature_matrix(test_fname)
-    out_fname = 'test_eval_l_' + str(lambda_reg) + '_p_' + str(p) + '_tr_' + str(tr_pts) + '.csv'
-    sgd_model.test_printer(test_id_list, test_feature_mat, out_fname)
+#     # pat = re.compile(r'(\d*)-(\d*)-(\d*)\s(\d*):(\d*):(\d*)')
+#     # plotter()
+#     # pdb.set_trace()
+#     fname = '../data/train.csv'
+#     # fname = '../data/test_features.csv'
+#     feature_matrix = get_feature_matrix(fname)
+#     out_vec = get_output(fname)
+#     lambda_reg = 1
+#     p = 1
+#     # weight_vector = get_weight_vector(feature_matrix, out_vec, lambda_reg, p)
+#     # Regression with p-norm regularization
+#     # loss = ||y - y'||^2 + lambda * ||w||^p
+#     # Can try to use k-fold cross validation
+#     # to check if the sgd code is indeed working
+#     # do a sanity check for the case p=2
+#     # sgd_model = model(lamb, p, all_data)
+#     tot_points = feature_matrix.shape[0]
+#     tr_pts = int(tot_points * 0.9)
+#     sgd_model = model(feature_matrix[:tr_pts], out_vec[:tr_pts], lambda_reg, p)
+#     sgd_model.p2_direct_solve()
+#     # sgd_model.sgd(1000, 1000)
+#     sgd_model.p1_solver()
+#     print(sgd_model.complete_loss())  #
+#     # test_feature_mat = get_feature_matrix('../data/test_features.csv')
+#     # sgd_model.p2_direct_solve()
+#     sgd_model.valid_accuracy(feature_matrix[tr_pts:], out_vec[tr_pts:])
+#     # sgd_model.valid_accuracy(feature_matrix, out_vec)
+#     test_fname = '../data/test_features.csv'
+#     test_data_reader = pd.read_csv(test_fname, sep=',')
+#     test_id_list = test_data_reader['Id']
+#     test_feature_mat = get_feature_matrix(test_fname)
+#     out_fname = 'test_eval_l_' + str(lambda_reg) + '_p_' + str(p) + '_tr_' + str(tr_pts) + '.csv'
+#     sgd_model.test_printer(test_id_list, test_feature_mat, out_fname)
