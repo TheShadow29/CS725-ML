5b44d8c094eba1817f572de9a5e85f904eb43929
Arka Sadhu <ark.sadhu2904@gmail.com>
2017-10-27 10:46:19 +0530

    I think feed forward is working properly hopefully

--- Assignment2/code/main_code.py
+++ Assignment2/code/main_code.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import pdb
 
 
 class data_loader(object):
@@ -12,7 +13,9 @@ class data_loader(object):
     def next_item(self, num=1, columns='all'):
         # if columns == 'all':
         if self.curr_ind + num < self.last_loc:
-            out_dat = self.csv_data.loc[self.curr_ind:self.curr_ind + num]
+            # pdb.set_trace()
+            # Note : .loc[a:b] gives a and b inclusive
+            out_dat = self.csv_data.loc[self.curr_ind:self.curr_ind + num - 1]
             self.curr_ind += num
         else:
             out_dat = self.csv_data.loc[self.curr_ind:]
@@ -29,68 +32,61 @@ def cross_entropy(actual_data, pred_data):
     one_hot_vec_len = pred_data.shape[1]
     batch_s = pred_data.shape[0]
     out1 = np.log(pred_data)
-    actual_data_one_hot = np.zeros(batch_s, one_hot_vec_len)
+    actual_data_one_hot = np.zeros((batch_s, one_hot_vec_len))
     actual_data_one_hot[:, actual_data] = 1
     out2 = -np.multiply(actual_data_one_hot, out1)
     return np.sum(out2)
 
 
-class node(object):
-    def __init__(self, gradient_back=0, curr_val=0):
-        self.curr_val = curr_val
-        self.gradient_back = gradient_back
-        return
+# def pre_process(inp_data):
 
-    def reset_grad(self):
-        self.gradient_back = 0
+# class node(object):
+#     def __init__(self, gradient_back=0, curr_val=0):
+#         self.curr_val = curr_val
+#         self.gradient_back = gradient_back
+#         return
 
-    def reset_curr_val(self):
-        self.curr_val = 0
+#     def reset_grad(self):
+#         self.gradient_back = 0
 
-    def reset_node(self):
-        self.gradient_back = 0
-        self.curr_val = 0
+#     def reset_curr_val(self):
+#         self.curr_val = 0
+
+#     def reset_node(self):
+#         self.gradient_back = 0
+#         self.curr_val = 0
 
 
-class layer(object):
-    def __init__(self, num_nodes=5):
-        self.node_list = list()
-        for _ in range(num_nodes):
-            new_node = node()
-            self.node_list.append(new_node)
-
-    def reset_layer_grad(self):
-        for n in self.node_list:
-            n.reset_grad()
-
-    def reset_layer_curr_val(self):
-        for n in self.node_list:
-            n.reset_curr_val()
-        return
-
-    def reset_layer(self):
-        for n in self.node_list:
-            n.reset_node()
+class lin_layer(object):
+    def __init__(self, inp_nodes, out_nodes):
+        # self.node_list = list()
+        # for _ in range(num_nodes):
+        #     new_node = node()
+        #     self.node_list.append(new_node)
+        # self.tot_nodes = len(self.node_list)
+        self.inp_nodes = inp_nodes
+        self.out_nodes = out_nodes
+        self.W = np.zeros((self.out_nodes, self.inp_nodes))
+        self.b = np.zeros(self.out_nodes)
         return
 
     def weights_l2(self):
-        norm = 0
-        for n in self.node_list:
-            norm += n.curr_val**2
-        return np.sqrt(norm)
+        return np.linalg.norm(self.W, 'fro') + np.linalg.norm(self.b)
+        # norm = 0
+        # for n in self.node_list:
+        #     norm += n.curr_val**2
+        # return np.sqrt(norm)
 
-
-class lin_layer(layer):
-    def __init__(self, num_nodes=5):
-        super(lin_layer, self).__init__()
+    def forward(self, inp):
+        return np.dot(self.W, inp) + self.b
 
 
 class neural_network(object):
-    def __init__(self):
+    def __init__(self, inp_nodes):
         self.list_layers = list()
-        fc1 = lin_layer()
+        fc1 = lin_layer(inp_nodes, 50)
         self.list_layers.append(fc1)
-        fc2 = lin_layer()
+        fc2 = lin_layer(50, 10)
         self.list_layers.append(fc2)
         return
 
@@ -99,6 +95,27 @@ class neural_network(object):
         for l in self.list_layers:
             norm += l.weights_l2() ** 2
         return np.sqrt(norm)
+
+    def softmax(self, inp):
+        # Assume inp is an array
+        n_t = np.exp(inp)
+        z_t = np.sum(n_t)
+        return n_t / z_t
+
+    def feed_forward(self, inp):
+        # pdb.set_trace()
+        out = self.list_layers[0].forward(inp)
+        out = self.list_layers[1].forward(out)
+        out = self.softmax(out)
+        return out
+
+    def get_output(self, inp):
+        rows = inp.shape[0]
+        out_list = list()
+        for r in range(rows):
+            out = self.feed_forward(inp[r, :])
+            out_list.append(out)
+        return np.array(out_list)
 
 
 class model(object):
@@ -111,14 +128,23 @@ class model(object):
 
     def train_net(self, num_epoch=15, batch_size=4):
         num_train_data = self.train_data.last_loc + 1
-        for epoch in num_epoch:
+        for epoch in range(num_epoch):
             for _ in np.arange(0, num_train_data, batch_size):
                 train_data_new = self.train_data.next_item(num=batch_size)
                 x_train_data = train_data_new[:, :-1]
                 y_train_data = train_data_new[:, -1]
                 y_pred_data = self.nn.get_output(x_train_data)
+                # pdb.set_trace()
                 curr_loss = self.loss(y_train_data, y_pred_data)
-                self.nn.update_weights()
+                # self.nn.update_weights()
+                print(curr_loss)
+
+    # def test_net(self):
+    #     num_corr = 0
+    #     tot_num = 0
+    #     num_train_data = self.train_data.last_loc + 1
+    #     for _ in np.arange(0, num_train_data, batch_size):
+    #         test_data_new
 
     def loss(self, actual_data, pred_data):
         ce_loss = cross_entropy(actual_data, pred_data)
@@ -131,7 +157,7 @@ if __name__ == '__main__':
     test_fname = '../data/test.csv'
     train_data = data_loader(train_fname)
     test_data = data_loader(test_fname)
-    simple_net = neural_network()
+    simple_net = neural_network(10)
     simple_model = model(simple_net, train_data, test_data)
     # train_csv_data = pd.read_csv(train_fname, sep=',')
     # test_csv_data = pd.read_csv(test_fname, sep=',')
